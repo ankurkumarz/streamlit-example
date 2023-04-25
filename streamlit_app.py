7e51b9aed295f33fb4780443fad4ca100e7eb400
Ankur Kumar <ankurkumar78@gmail.com>
2023-04-25 11:16:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,38 @@
-
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
 import streamlit as st
-from streamlit_chat import message
+
+"""
+# Welcome to Streamlit!
+
+Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+
+If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+forums](https://discuss.streamlit.io).
+
+In the meantime, below is an example of what you can do with just a few lines of code:
+"""
 
 
-# This function uses the OpenAI Completion API to generate a 
-# response based on the given prompt. The temperature parameter controls 
-# the randomness of the generated response. A higher temperature will result 
-# in more random responses, 
-# while a lower temperature will result in more predictable responses.
-def generate_response(prompt):
-    message = "hello"
-    return message
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
+    Point = namedtuple('Point', 'x y')
+    data = []
 
-st.title("🤖 chatBot : openAI GPT-3 + Streamlit")
+    points_per_turn = total_points / num_turns
 
+    for curr_point_num in range(total_points):
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+        radius = curr_point_num / total_points
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
+        data.append(Point(x, y))
 
-if 'generated' not in st.session_state:
-    st.session_state['generated'] = []
-
-if 'past' not in st.session_state:
-    st.session_state['past'] = []
-
-
-def get_text():
-    input_text = st.text_input("You: ","Hello, how are you?", key="input")
-    return input_text 
-
-
-user_input = get_text()
-
-if user_input:
-    output = generate_response(user_input)
-    st.session_state.past.append(user_input)
-    st.session_state.generated.append(output)
-
-if st.session_state['generated']:
-
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
-        message(st.session_state["generated"][i], key=str(i))
-        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
